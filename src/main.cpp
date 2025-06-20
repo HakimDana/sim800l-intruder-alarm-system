eefafb52c5c72ca5e8b5b4826382a3e7815c1185
hakim <hakimdana88@gmail.com>
2025-06-20 19:11:46 +0330

    added goals.md and write,read,delete ability for numbers in eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,23 @@ void updateSerial();
 void handlealarm(bool *alarmstateptr);
 void handlecommand();
 void sendsms(String sms, String phonenumber);
+void readWhitelist();
 
 String checkstats();
 String cutstring(String text, int starti, int endi);
 
 bool CompareFirstofString(String inputString, String stringtoCompare);
-bool checkMessenger(String *phonenumber);
+bool checkMessenger(String phonenumber);
 bool writeNumberToEeprom(String number);
+bool deleteNumberInEeprom(String number);
 
 unsigned char calcChecksum(String inputString);
 
 struct storedNumber
-  {
-    char phoneNumber[13];
-    unsigned char checksum = 0;
-  };
+{
+  char phoneNumber[13];
+  unsigned char checksum = 0;
+};
 
 // Create software serial object to communicate with SIM800L
 SoftwareSerial mySerial(3, 2); // SIM800L Tx & Rx is connected to Arduino #3 & #2
@@ -37,16 +39,42 @@ bool enable = true;
 unsigned long dialstart = 0;
 unsigned long alarmStart = 0;
 String sim800lbuff = "";
-String NumberWhiteList[] = {"989029026240"};
+String NumberWhiteList[70];
+
+unsigned char numberWhiteListLength = 0;
 
 String *sim800lbuffptr = &sim800lbuff;
 bool *newMesptr = &newMes;
 bool *enableptr = &enable;
 
+void printwholeeeprom()
+{
+  for (int i = 0; i < EEPROM.length(); i += 14)
+  {
+    for (int index = 0; index < 14 && i + index < EEPROM.length(); index++)
+    {
+      Serial.print((char)EEPROM[i + index]);
+      Serial.print(" ");
+    }
+    Serial.println("");
+  }
+}
+
+void printwholewhitelist()
+{
+  for (int i = 0; i < numberWhiteListLength; i++)
+  {
+
+    Serial.print(NumberWhiteList[i]);
+    Serial.print(" ");
+  }
+  Serial.println(" aga white listo printidam");
+}
+
 void setup()
 {
-  pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
   // Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
 
@@ -67,21 +95,24 @@ void setup()
   updateSerial();
   mySerial.println("AT+CNMI=1,2,0,0,0");
   updateSerial();
-  
+  // erase eeprom:
   // for (int i = 0; i < EEPROM.length(); i++)
-  // {
-    //   EEPROM[0] = 255;
-    // }
-    
-  // writeNumberToEeprom(NumberWhiteList[0]);
+  //{
+  //  EEPROM.update(i, 255);
+  //}
 
-  storedNumber testnum;
-
-  EEPROM.get(0,testnum);
-  Serial.println(testnum.phoneNumber);
-  Serial.println(testnum.checksum);
-  Serial.println("i have finished printing stuff, entering the loop");
-  
+  // Serial.println(writeNumberToEeprom("985138442196"));
+  // readWhitelist();
+  // printwholewhitelist();
+  // Serial.println(writeNumberToEeprom("985138442196"));
+  // readWhitelist();
+  deleteNumberInEeprom("985138442196");
+  writeNumberToEeprom("989029026240");
+  printwholeeeprom();
+  readWhitelist();
+  printwholewhitelist();
+  Serial.print("numberwhitelistlength is: ");
+  Serial.println(numberWhiteListLength);
 }
 void loop()
 {
@@ -139,7 +170,7 @@ void loop()
 
         mySerial.println("ATH");
         updateSerial();
-      }     
+      }
     }
 
     executed = true;
@@ -196,52 +227,50 @@ void handlecommand()
 {
   String Senderphonenumber = cutstring(sim800lbuff, 11, 21);
 
-  if (checkMessenger(&Senderphonenumber) == true)
+  if (checkMessenger(Senderphonenumber) == true)
   {
     Serial.println("number matched");
 
     String sms = "";
-  if (sim800lbuff[2] == '+' && sim800lbuff[3] == 'C' && sim800lbuff[4] == 'M' && sim800lbuff[5] == 'T')
-  {
-
-    for (unsigned int i = 10; i < sim800lbuff.length() - 1; i++)
+    if (sim800lbuff[2] == '+' && sim800lbuff[3] == 'C' && sim800lbuff[4] == 'M' && sim800lbuff[5] == 'T')
     {
-      if (sim800lbuff[i] == '\n')
-      {
-        sms = cutstring(sim800lbuff, i + 2, sim800lbuff.length());
-        Serial.print("there is your sms:");
-        Serial.println(sms);
 
-        if (CompareFirstofString(sms, "enable") == true)
+      for (unsigned int i = 10; i < sim800lbuff.length() - 1; i++)
+      {
+        if (sim800lbuff[i] == '\n')
         {
-          Serial.println("enable command detected");
-          *enableptr = true;
+          sms = cutstring(sim800lbuff, i + 2, sim800lbuff.length());
+          Serial.print("there is your sms:");
+          Serial.println(sms);
+
+          if (CompareFirstofString(sms, "enable") == true)
+          {
+            Serial.println("enable command detected");
+            *enableptr = true;
+          }
+          else if (CompareFirstofString(sms, "disable") == true)
+          {
+            Serial.println("disable command detected");
+            *enableptr = false;
+          }
+          else if (CompareFirstofString(sms, "stats") == true)
+          {
+            Serial.println("stats command detected");
+            Serial.println(checkstats());
+          }
+          else if (CompareFirstofString(sms, "alarm off") == true)
+          {
+            Serial.println("alarm off command detected");
+          }
+          else if (CompareFirstofString(sms, "add number"))
+          {
+            Serial.println("add number command detected");
+          }
+          continue;
         }
-        else if (CompareFirstofString(sms, "disable") == true)
-        {
-          Serial.println("disable command detected");
-          *enableptr = false;
-        }
-        else if (CompareFirstofString(sms, "stats") == true)
-        {
-          Serial.println("stats command detected");
-          Serial.println(checkstats());
-        }
-        else if (CompareFirstofString(sms, "alarm off") == true)
-        {
-          Serial.println("alarm off command detected");
-        }else if (CompareFirstofString(sms,"add number")){
-          Serial.println("add number command detected");  
-          
-        }
-        continue;
       }
     }
   }
-
-  }
-
-  
 }
 // returns a string starting and ending from the given index numbers in the given string
 String cutstring(String text, int starti, int endi)
@@ -268,13 +297,13 @@ bool CompareFirstofString(String inputString, String stringtoCompare)
   return true;
 }
 // compares the senders phone number to the given number
-bool  checkMessenger(String *phonenumber)
+bool checkMessenger(String phonenumber)
 {
-  //String phonenumber = cutstring(sim800lbuff, 11, 21);
+  // String phonenumber = cutstring(sim800lbuff, 11, 21);
 
-  for (int i = 0; i < sizeof(NumberWhiteList) / sizeof(NumberWhiteList[0]); i++)
+  for (int i = 0; i < numberWhiteListLength; i++)
   {
-    if (*phonenumber == NumberWhiteList[i])
+    if (phonenumber == NumberWhiteList[i])
     {
       return true;
     }
@@ -321,32 +350,21 @@ unsigned char calcChecksum(String inputString)
   unsigned char checksum = 0;
 
   for (int i = 0; i < inputString.length(); i++)
-   {
+  {
     checksum += inputString[i];
-   }
+  }
 
-   return checksum;
+  return checksum;
 }
 
-
-String readWhitelist(){
-
-}
-
-bool writeNumberToEeprom(String number)
+void readWhitelist()
 {
-   int index = 0;
-   storedNumber  tempStoredNumber;
-   number.toCharArray(tempStoredNumber.phoneNumber,13);
-   tempStoredNumber.checksum = calcChecksum(tempStoredNumber.phoneNumber);
-   
-   
-  Serial.println(tempStoredNumber.phoneNumber);
-  Serial.println(tempStoredNumber.checksum);
+  numberWhiteListLength = 0;
+  storedNumber tempReadNumber;
 
-const int STRUCT_SIZE = sizeof(tempStoredNumber);
+  const int STRUCT_SIZE = sizeof(tempReadNumber);
 
-  for (int index = 0; index < EEPROM.length(); index+= STRUCT_SIZE)
+  for (int index = 0; index < EEPROM.length(); index += STRUCT_SIZE)
   {
     bool isEmpty = false;
     for (int slotIndex = 0; slotIndex < STRUCT_SIZE; slotIndex++)
@@ -354,18 +372,119 @@ const int STRUCT_SIZE = sizeof(tempStoredNumber);
       if (EEPROM[slotIndex + index] != 0xff)
       {
         isEmpty = false;
-      }else{
+        break;
+      }
+      else
+      {
+        isEmpty = true;
+      }
+    }
+    if (isEmpty == false)
+    {
+      EEPROM.get(index, tempReadNumber);
+      if (tempReadNumber.checksum == calcChecksum(tempReadNumber.phoneNumber))
+      {
+        NumberWhiteList[numberWhiteListLength] = tempReadNumber.phoneNumber;
+        numberWhiteListLength++;
+      }
+    };
+  }
+  Serial.print("number white list inside the function: ");
+  printwholewhitelist();
+}
+
+bool writeNumberToEeprom(String number)
+{
+  if (number.length() != 12)
+  {
+    Serial.println("bad format :(");
+    return true;
+  }
+
+  readWhitelist();
+
+  for (int i = 0; i < numberWhiteListLength; i++)
+  {
+    Serial.println("man to in for e am");
+    if (number == NumberWhiteList[i])
+    {
+      Serial.println("number is already stored!!");
+      return true;
+    }
+  }
+
+  storedNumber tempStoredNumber;
+  number.toCharArray(tempStoredNumber.phoneNumber, 13);
+  tempStoredNumber.checksum = calcChecksum(tempStoredNumber.phoneNumber);
+
+  Serial.println(tempStoredNumber.phoneNumber);
+  Serial.println(tempStoredNumber.checksum);
+
+  const int STRUCT_SIZE = sizeof(tempStoredNumber);
+
+  for (int index = 0; index < EEPROM.length(); index += STRUCT_SIZE)
+  {
+
+    bool isEmpty = true;
+    for (int slotIndex = 0; slotIndex < STRUCT_SIZE; slotIndex++)
+    {
+
+      if (EEPROM[slotIndex + index] != 0xff)
+      {
+        isEmpty = false;
+      }
+      else
+      {
         isEmpty = true;
         break;
       }
-      
     }
     if (isEmpty)
     {
-      EEPROM.put(index,tempStoredNumber);
-      break;
+      EEPROM.put(index, tempStoredNumber);
+      return false;
     }
-    
-    
   }
+  Serial.println("not enough space on eeprom!!");
+  return true;
+}
+
+bool deleteNumberInEeprom(String number)
+{
+
+  numberWhiteListLength = 0;
+  storedNumber tempReadNumber;
+
+  const int STRUCT_SIZE = sizeof(tempReadNumber);
+
+  for (int index = 0; index < EEPROM.length(); index += STRUCT_SIZE)
+  {
+    bool isEmpty = false;
+    for (int slotIndex = 0; slotIndex < STRUCT_SIZE; slotIndex++)
+    {
+      if (EEPROM[slotIndex + index] != 0xff)
+      {
+        isEmpty = false;
+        break;
+      }
+      else
+      {
+        isEmpty = true;
+      }
+    }
+    if (isEmpty == false)
+    {
+      EEPROM.get(index, tempReadNumber);
+      if (number == tempReadNumber.phoneNumber)
+      {
+        for (int i = 0; i < STRUCT_SIZE; i++)
+        {
+          EEPROM.write(i + index, 255);
+        }
+        return 0;
+      }
+    }
+  }
+  Serial.println("shomare ro piyda nakardom");
+  return true;
 }
